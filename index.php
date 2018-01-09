bfeeae223b7e454f48603bb24b88617994d125cc
alfieteam <alfieteam1993@gmail.com>
2018-01-09 22:03:04 +0200

    Game v0.0.1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Game v 0.0.1</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="header">
+	Шапка
+</div>
+<div id="container">
+	<div id="navigation">Навигация</div>
+	<div id="content">Контент<br><br></div>
+</div>
+<div id="footer">
+	Подвал
+</div>
+
+</body>
+</html>
