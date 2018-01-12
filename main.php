1c37db12c8ccf2385591a018e2995416986f107a
alfieteam <alfieteam1993@gmail.com>
2018-01-12 20:36:33 +0200

    Game v0.0.6

--- main.php
+++ main.php
@@ -5,16 +5,58 @@ include("header.php");
 if(!isset($_SESSION['uid'])){
 	echo "Вы не авторизированы, авторизируйтесь!";
 }elseif(isset($_SESSION['uid'])){
-	echo "Username: ".$_SESSION['username']." | Ваш ID: ".$_SESSION['uid'];
-	echo "<br><hr>";
-	echo "Gold: ".$stats['gold']." | Attack: ".$stats['attack']." | Defense: ".$stats['defense']." | Food: ".$stats['food'];
-	echo "<br><hr>";
-	echo "Worker: ".$unit['worker']." | Farmer: ".$unit['farmer']." | Warrior: ".$unit['warrior']." | Defender: ".$unit['defender'];
-	echo "<br><hr>";
-	echo "Sword: ".$weapon['sword']." | Shield: ".$weapon['shield'];
+?>
 
+	<center><h2>Your States</h2></center>
+	<hr>
+	<table cellpadding="3" cellspacing="5">
+		<tr>
+			<td>Username:</td>
+			<td><i><?php echo $user['username'];?></i></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Attack:</td>
+			<td><i><?php echo $stats['attack'];?></i></td>
+		</tr>
+		<tr>
+			<td>Defense:</td>
+			<td><i><?php echo $stats['defense'];?></i></td>
+		</tr>
+		<tr>
+			<td>Gold:</td>
+			<td><i><?php echo $stats['gold'];?></i></td>
+		</tr>
+		<tr>
+			<td>Food:</td>
+			<td><i><?php echo $stats['food'];?></i></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Workers:</td>
+			<td><i><?php echo $unit['worker'];?></i></td>
+		</tr>
+		<tr>
+			<td>Farmers:</td>
+			<td><i><?php echo $unit['farmer'];?></i></td>
+		</tr>
+		<tr>
+			<td>Warriors:</td>
+			<td><i><?php echo $unit['warrior'];?></i></td>
+		</tr>
+		<tr>
+			<td>Defenders:</td>
+			<td><i><?php echo $unit['defender'];?></i></td>
+		</tr>
+	</table>
 
-
+<?php
 
 }
 
